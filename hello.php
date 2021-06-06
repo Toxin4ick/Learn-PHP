3c0e75629e483eb9722dd13af7b95eca76b1df14
Toxin4ick <toks0@mail.ru>
2021-06-06 17:23:40 +1000

    Variables

--- hello.php
+++ hello.php
@@ -19,7 +19,24 @@
     echo "<h2>Заголовок параграфа</h2>";
     echo "Текст параграфа";
     ?>
-    2 + 2 =  <?= 2+2 ?>
+    2 + 2 =  <?= 2+2 ?> <br />
+    <?php
+    // define the $ num variable
+    $num = 10;
+    // output the value of the variable $ num to the web page
+    echo $num."<br/>";
+
+    // change the value of the variable
+    $num = 22;
+    echo $num."<br/>";
+    //assigning a value to another variable
+    $a = 15;
+    $b = $a;
+    echo $b;
+    //check what happens if you do not assign a value
+    $numerror;
+    echo $numerror;
+    ?>
 </div>
 </body>
 </html>
